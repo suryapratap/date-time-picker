1cfe09e97e45eb20ee9c6074532a8ffb5e2e82d7
Surya Pratap <suryapratap@users.noreply.github.com>
2021-06-05 10:32:19 +0530

    angular v 12.0.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import * as dayjs from 'dayjs';
 import * as timezone from 'dayjs/plugin/timezone';
@@ -12,21 +14,22 @@ dayjs.extend(timezone);
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.sass']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit {
     @ViewChild('date_range_component', { static: true })
     date_range_component: OwlDateTimeComponent<AppComponent>;
-    public selectedDates = [
-        dayjs('2019-03-11T00:00:00Z'),
-        dayjs('2019-03-11T15:00:00Z')
-    ];
+    public selectedDates = [];
 
 
-    currentValue: Date = this.selectedDates[0]?.toDate();
-    endValue: Date = this.selectedDates[1]?.toDate();
+    currentValue: Date;
+    endValue: Date;
 
     open_once = true;
+    ngOnInit() {
+        this.currentValue = dayjs('2019-03-11T00:00:00Z').toDate();
+        this.endValue = dayjs('2019-03-11T15:00:00Z').toDate();
+        this.selectedDates[0] = dayjs(this.currentValue);
+        this.selectedDates[1] = dayjs(this.endValue);
 
-    ngAfterViewInit() {
     }
 
     recordChanges(event) {
